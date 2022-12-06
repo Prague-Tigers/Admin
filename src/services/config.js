d878e7f004a966ceef2ddfee2ba81cd64d840949
Jakub Štellner <stelljak@fit.cvut.cz>
2022-12-06 19:18:01 +0100

    admin: update

--- src/services/config.js
+++ src/services/config.js
@@ -1,25 +1,3 @@
-import { QueryClient } from "react-query"
+const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://app-production-39e2.up.railway.app"
 
-let medusaUrl = "http://localhost:9000"
-
-// deprecated
-if (process.env.GATSBY_STORE_URL) {
-  medusaUrl = process.env.GATSBY_STORE_URL
-}
-
-// takes precedence over GATSBY_STORE_URL
-if (process.env.GATSBY_MEDUSA_BACKEND_URL) {
-  medusaUrl = process.env.GATSBY_MEDUSA_BACKEND_URL
-}
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      staleTime: 90000,
-      retry: 1,
-    },
-  },
-})
-
-export { medusaUrl, queryClient }
+export { medusaUrl }
